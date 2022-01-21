a271bd7f22adb3c0d89684097c2a981c5401ca68
BojanPericBP <bojan.peric67@gmail.com>
2022-01-21 18:03:51 +0100

    Objavljivanje prakse

--- src/app/firma/components/objava-prakse/objava-prakse.component.ts
+++ src/app/firma/components/objava-prakse/objava-prakse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-objava-prakse',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjavaPrakseComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  vrstaPrakse!: FormGroup;
+
+  campaignOne: FormGroup;
+  campaignTwo: FormGroup;
+
+  mentori = [
+    { id: 'id1', ime: 'Ime1', prezime: 'Prezime1' },
+    { id: 'id2', ime: 'Ime2', prezime: 'Prezime2' },
+    { id: 'id3', ime: 'Ime3', prezime: 'Prezime3' },
+  ];
+
+  constructor(private formBuilder: FormBuilder) {
+    const today = new Date();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+
+    this.campaignOne = new FormGroup({
+      start: new FormControl(new Date()),
+      end: new FormControl(new Date()),
+    });
+
+    this.campaignTwo = new FormGroup({
+      start: new FormControl(new Date()),
+      end: new FormControl(new Date()),
+    });
   }
 
+  ngOnInit(): void {
+    this.firstFormGroup = this.formBuilder.group({
+      'nazivPrakse': new FormControl("", [Validators.required])
+    });
+
+    this.secondFormGroup = this.formBuilder.group({
+      'smjer': new FormControl(),
+      'SI': new FormControl(false),
+      'RI': new FormControl(false),
+      'EL': new FormControl(false),
+      'TEL': new FormControl(false),
+      'vrstaPrakse': new FormControl('', Validators.required),
+      'brojSati':new FormControl(150)
+    });
+    let praksa;
+    if((praksa = this.secondFormGroup.get('vrstaPrakse'))!= null)
+      praksa.setValue("strucna");
+  }
+  
 }
--- src/app/firma/firma.module.ts
+++ src/app/firma/firma.module.ts
@@ -5,13 +5,38 @@ import { FirmaRoutingModule } from './firma-routing.module';
 import { ObjavaPrakseComponent } from './components/objava-prakse/objava-prakse.component';
 
 
+import { MatButtonModule} from '@angular/material/button';
+import { MatStepperModule} from '@angular/material/stepper';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+// import {} from '@angular/material/';
+
+
 @NgModule({
   declarations: [
-    ObjavaPrakseComponent
+    ObjavaPrakseComponent,
   ],
   imports: [
     CommonModule,
-    FirmaRoutingModule
+    FirmaRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers:[
+    MatDatepickerModule
   ]
 })
 export class FirmaModule { }
