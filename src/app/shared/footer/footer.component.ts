76e642672535d6c46f9d7aef8def2cbf5b4b8b63
SasaStankovic <sasa.stankovic.ss@gmail.com>
2022-08-29 12:04:49 +0200

    footer fix

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  adminMail: string = "admin@mail.com";
+
   constructor() { }
 
   ngOnInit(): void {
